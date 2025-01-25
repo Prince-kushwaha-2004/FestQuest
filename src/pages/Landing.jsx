9cfa12d7bf9828dfee888c36ae6f78ef292dba85
Prince <prince121kk@gmail.com>
2025-01-25 15:59:59 +0530

    Changed Landing Page UI

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,29 @@
+import { CaretDownOutlined } from '@ant-design/icons';
+import { Dropdown, Space } from 'antd';
 import React from "react";
-
+import { Link } from "react-router-dom";
 const LandingPage = () => {
+  const items = [
+    {
+      key: '1',
+      label: (
+        <Link to="register">Register as user</Link>
+
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <Link to="login">Register as organisation</Link>
+
+      ),
+    },
+
+  ];
   return (
-    <div className="bg-gradient-to-br from-[#e6f3ff] via-[#f0f9e6] to-[#fff0f4] min-h-screen">
-      <nav className="bg-white/80 backdrop-blur-sm shadow-md">
+
+    <div className="min-h-screen flex flex-col">
+      <nav className="bg-white/80 backdrop-blur-sm">
         <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
           <div className="flex items-center">
             <img
@@ -13,27 +33,31 @@ const LandingPage = () => {
             />
           </div>
           <div className="flex items-center space-x-4">
-            <a
-              href="#"
-              className="px-4 py-2 text-indigo-600 hover:bg-indigo-50 rounded-full"
-            >
+            <Link to="login" className="px-6 py-2 bg-[#00637C] text-white rounded-full hover:bg-[#00637cc0] ">
               Login
-            </a>
-            <a
-              href="#"
-              className="px-4 py-2 bg-indigo-500 text-white rounded-full hover:bg-indigo-600 "
-            >
-              Sign Up
-            </a>
+            </Link>
+            <Space direction="vertical">
+              <Space wrap>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                  placement="bottomRight"
+                >
+                  <button className="px-4 py-2 text-[#00637C] hover:bg-indigo-50 rounded-full"> Sign Up <CaretDownOutlined /></button>
+                </Dropdown>
+              </Space>
+            </Space>
+
           </div>
         </div>
       </nav>
 
-      <div className="max-w-screen-xl px-4 sm:px-6 lg:px-8 mx-auto py-16 flex flex-col items-center justify-center">
-        <h1 className="text-4xl sm:text-6xl font-bold text-gray-800 mb-4 text-center leading-tight">
-          Think, plan, and track all in one place
+      <div className="m-4 mt-0 border-2 border-slate-200 flex-1 bg-gradient-to-br from-[#e6f3ff] via-[#f0f9e6] to-[#fff0f4] rounded-4xl px-4 md:px-6 lg:px-60 2xl:px-80 py-16 flex flex-col items-center justify-center">
+        <h1 className="text-4xl md:text-6xl xl:text-8xl font-semibold text-gray-800 mb-4 text-center leading-tight">
+          Think, plan, and track <br /><span className="text-neutral-400"> all in one place</span>
         </h1>
-        <p className="text-lg sm:text-xl text-gray-600 mb-8 text-center max-w-2xl">
+        <p className="text-lg sm:text-xl md:text-2xl text-slate-700 mb-8 text-center max-w-2xl">
           Efficiently manage your tasks and boost productivity with our
           intuitive event planning platform.
         </p>
