d7f2d92c308a4e23361b168d9c530dd34d4a556b
Prince <prince121kk@gmail.com>
2025-01-26 12:31:57 +0530

    update routing and dashboard page

---
+++ src/pages/User/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <>
+            <div className='bg-yellow-200 flex-1'>
+
+            </div>
+            <div className='bg-pink-400 w-[20rem]'>nav2</div>
+        </>
+    )
+}
+
+export default Feed
