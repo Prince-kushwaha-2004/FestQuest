23c70874fd10404630c4bb687059f28677ba259b
shivanshu11092003 <sshivanshugupta11@gmail.com>
2025-01-26 15:00:36 +0530

    registerPage 1.2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,6 +5,7 @@ import logo from "../assets/festLogo.png";
 import Axios from "../axios/Axios";
 import HandleChange from "../utils/Function";
 
+
 const Register = () => {
 
   const [registerForm, setRegisterForm] = useState({
@@ -14,6 +15,8 @@ const Register = () => {
     password: "",
     confirmPassword: "",
   });
+
+  const [next, setNextState] = useState(0)
 
   const getOTP = () => {
     const request = {
@@ -26,13 +29,19 @@ const Register = () => {
   };
 
   const [current, setCurrent] = useState(0);
-  const onChange = (value) => {
-    console.log("onChange:", value);
-    setCurrent(value);
+  const onChange = (text) => {
+    console.log('onChange:', text);
+  };
+  const onInput = (value) => {
+    console.log('onInput:', value);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
   const description = "This is a description.";
+  const sharedProps = {
+    onChange,
+    onInput,
+  };
 
   return (
     <>
@@ -45,32 +54,44 @@ const Register = () => {
               size: "middle",
               paddingBlock: 7,
               inputFontSize: 18,
-              activeBorderColor: "#1677ff",
               paddingInline: 16,
+              activeBorderColor: "#00637C",
+              hoverBorderColor: "#FFBF61",
             },
             Button: {
               paddingInline: 29,
               paddingBlock: 36,
-              primaryColor: "#fff",
+              defaultBg: '#00637C'
+
             },
+            Steps: {
+              colorPrimary: '#00637C',
+              colorText: "#000",
+              colorTextDisabled: "#FFBF00",
+              colorTextQuaternary: "#FFBF00",
+              fontSize: 20,
+              lineHeight: 4
+            }
           },
         }}
       >
-        <div className="w-full justify-center h-screen flex items-center  bg-background p-18 ">
+        <div className="w-full justify-center h-screen flex items-center  bg-gradient-to-br from-[#e6f3ff] via-[#f0f9e6] to-[#fff0f4] p-18 ">
           <div className="drop-shadow-md static z-0 rounded-2xl self-center bg-white h-[90%] w-[93%] flex justify-end  ">
-            <div className=" w-2/3">
-              <div className="flex justify-end items-center px-5 font-semibold mt-[.5rem]  py-3 w-full">
-                <div className="px-4 ">Already have an account? </div>
-                <Button color="primary" variant="filled">
-                  Sign in
-                </Button>
-              </div>
+            <div className="w-1/3 flex flex-col items-center rounded-2xl p-10 bg-[url(../assets/bg10.jpeg"  >
 
-              <div className="flex justify-center text-4xl font-bold mt-11">
+
+
+            </div>
+            <div className="flex flex-col  items-center  w-2/3">
+
+
+
+
+              <div className="flex justify-center text-4xl font-bold text-themeColor mt-30">
                 Create your free account
               </div>
 
-              <div className="flex gap-4 mt-25 flex-col items-center  justify-center">
+              {current == 0 ? <div className="flex w-full gap-4 mt-25 flex-col items-center  justify-center">
                 <Input
                   name="name"
                   value={registerForm.name}
@@ -85,46 +106,95 @@ const Register = () => {
                   style={{ width: "50%" }}
                   placeholder="Email"
                 />
-                {/* <Input name='bio' value={registerForm.bio} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Bio" /> */}
-                {/* <Input name='password' value={registerForm.password} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Password" />
-                            <Input name='confirmPassword' value={registerForm.confirmPassword} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Confirm password" /> */}
+
                 <Button
-                  color="primary"
+                  color="default"
                   variant="solid"
-                  style={{}}
-                  onClick={(e) => console.log(registerForm)}
+                  onClick={(e) => {
+                    setCurrent(1);
+
+                  }}
                 >
                   next
                 </Button>
               </div>
-            </div>
-          </div>
-          <div className="absolute flex flex-col inset-y-0 left-0 opacity-70 justify-center items-end z-10 bg-themeColor h-full w-1/3  ">
-            <div className="w-1/3"></div>
+                : current == 1 ?
+                  <div className="flex w-full gap-4 mt-25 flex-col items-center  justify-center">
 
-            <div className=" w-2/3 pr-14 ">
-              <div className="  ">
-                {" "}
-                <img src={logo} alt="" />{" "}
+                    <Input.OTP formatter={(str) => str.toUpperCase()} {...sharedProps} />
+
+                    <div className="mt-14">
+                      <Button
+                        color="primary"
+                        variant="solid"
+                        onClick={(e) => setCurrent(2)
+                        }
+                      >
+                        next
+                      </Button>
+                    </div>
+                  </div> :
+                  current == 2 ? <div className="flex w-full gap-4 mt-25 flex-col items-center  justify-center">
+
+                    {/* <Input name='bio' value={registerForm.bio} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Bio" /> */}
+                    <Input name='password' value={registerForm.password} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Password" />
+                    <Input name='confirmPassword' value={registerForm.confirmPassword} onChange={e => HandleChange(e, setRegisterForm)} style={{ width: '50%' }} placeholder="Confirm password" />
+                    <Button
+                      color="primary"
+                      variant="solid"
+                      onClick={(e) => console.log(registerForm)}
+
+                    >
+                      Sign Up
+                    </Button>
+                  </div> : <></>
+
+              }
+
+              <div className="flex  justify-center mt-10 items-center px-8 font-semibold  w-full">
+                <div className="px-4  ">Already have an account? </div>
+                <Button color="default" variant="filled">
+                  Sign in
+                </Button>
+
               </div>
 
-              <div className="">
+            </div>
+
+
+          </div>
+          <div className="absolute flex flex-col inset-y-0 left-0 backdrop-invert backdrop-opacity-10  items-end z-10 bg-themeColor/10 h-full w-1/3  ">
+            <div className="w-1/3"></div>
+
+            <div className=" w-2/3 mt-[10rem] mr-8 ">
+
+
+
+
+              <div className="flex ml-7 w-[20rem]  ">
+
+                <img src={logo} alt="" />
+              </div>
+
+
+
+
+              <div className="flex pl-15 mt-20 items-center justify-center ">
                 <Steps
                   current={current}
-                  onChange={onChange}
                   direction="vertical"
                   items={[
                     {
                       title: "Step 1",
-                      description,
+                      description: "Email verification",
                     },
                     {
                       title: "Step 2",
-                      description,
+                      description: "OTP confirmation",
                     },
                     {
                       title: "Step 3",
-                      description,
+                      description: "Password and Confirm password",
                     },
                   ]}
                 />
@@ -132,7 +202,7 @@ const Register = () => {
             </div>
           </div>
         </div>
-      </ConfigProvider>
+      </ConfigProvider >
     </>
   );
 };
