80d9c4b93d2f3a070d0c915f3a0d3fa6495184de
Suhani <suhaniss885@gmail.com>
2025-01-27 15:30:34 +0530

    update

--- src/pages/organisation/OrganisationRegister.jsx
+++ src/pages/organisation/OrganisationRegister.jsx
@@ -1,6 +1,5 @@
 import { Button, ConfigProvider, Input, Select } from "antd";
 import { FcGoogle } from "react-icons/fc";
-import { FcGoogle } from "react-icons/fc";
 
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/festLogo.png";
@@ -19,18 +18,6 @@ const OrganisationRegister = () => {
     password: "",
     confirmPassword: "",
   });
-
-    const [registerForm, setRegisterForm] = useState({
-        name: "",
-        address: '',
-        Pincode: '',
-        type: '',
-        email: "",
-        bio: "",
-        password: "",
-        confirmPassword: "",
-    });
-
 
   const getOTP = () => {
     const request = {
@@ -59,36 +46,22 @@ const OrganisationRegister = () => {
 
   const items = [
     {
-      key: "1",
-      label: "School",
+      title: "Email verification",
+      description: "Email verification",
     },
     {
-      key: "2",
-      label: "College",
+      title: "Step 2",
+      description: "OTP confirmation",
     },
     {
-      key: "3",
-      label: "Other",
+      title: "Step 3",
+      description: "Password",
+    },
+    {
+      title: "Step 4",
+      description: "Address",
     },
   ];
-    const items = [
-        {
-            title: "Email verification",
-            description: "Email verification",
-        },
-        {
-            title: "Step 2",
-            description: "OTP confirmation",
-        },
-        {
-            title: "Step 3",
-            description: "Password",
-        },
-        {
-            title: "Step 4",
-            description: "Address",
-        },
-    ];
 
   return (
     <>
@@ -128,76 +101,23 @@ const OrganisationRegister = () => {
         >
           <div className="absolute  flex-col lg:flex hidden justify-center inset-y-0 left-0 backdrop-invert backdrop-opacity-10  items-end z-10 bg-themeColor/10 h-full w-1/3  ">
             <div className=" w-2/3  ">
-              <StepwiseProcess logo={logo} current={current} />
+              <StepwiseProcess
+                logo={logo}
+                itemsArray={items}
+                current={current}
+              />
             </div>
           </div>
           <div className="drop-shadow-md static z-0 rounded-2xl self-center   bg-[#2847270e] shadow border border-slate-300   p-5 h-[90%] w-[94%] flex justify-end  ">
             {/* <div className="w-1/2  md:w-1/3 p-4 hidden sm:flex flex-col justify-center items-center lg:hidden">
-    return (
-        <>
-
-            <div className="w-full justify-center h-screen flex items-center   bg-gradient-to-br from-[#e6f3ff] via-[#f0f9e6] to-[#fff0f4] md:p-10  ">
-                <ConfigProvider
-                    theme={{
-                        components: {
-                            Input: {
-                                colorPrimary: "#1677ff",
-                                inputFontSizeLG: 24,
-                                size: "middle",
-                                paddingBlock: 7,
-                                inputFontSize: 18,
-                                paddingInline: 16,
-                                activeBorderColor: "#00637C",
-                                hoverBorderColor: "#FFBF61",
-                            },
-                            Button: {
-                                paddingInline: 29,
-                                paddingBlock: 36,
-                                defaultBg: '#00637C'
-
-                            },
-                            Steps: {
-                                colorPrimary: '#00637C',
-                                colorText: "#000",
-                                colorTextDisabled: "#FFBF00",
-                                colorTextQuaternary: "#FFBF00",
-                                fontSize: 20,
-                                lineHeight: 3.8
-                            },
-                            Select: {
-                                activeBorderColor: "#00637C",
-                                hoverBorderColor: "#FFBF61",
-
-                            }
-                        },
-                    }}
-                >
-                    <div className="absolute  flex-col lg:flex hidden justify-center inset-y-0 left-0 backdrop-invert backdrop-opacity-10  items-end z-10 bg-themeColor/10 h-full w-1/3  ">
-
-                        <div className=" w-2/3  ">
-
-                            <StepwiseProcess logo={logo} itemsArray={items} current={current} />
-
-
-                        </div>
-                    </div>
-                    <div className="drop-shadow-md static z-0 rounded-2xl self-center   bg-[#2847270e] shadow border border-slate-300   p-5 h-[90%] w-[94%] flex justify-end  ">
-
-                        {/* <div className="w-1/2  md:w-1/3 p-4 hidden sm:flex flex-col justify-center items-center lg:hidden">
                             <StepwiseProcess logo={logo} current={current} />
 
-                        </div> */}
                         </div> */}
 
             <div className="flex flex-col justify-center items-center gap-20  lg:w-2/3 w-full">
               <div className="flex justify-center  sm:text-4xl text-3xl font-bold text-themeColor ">
                 Create your free account
               </div>
-                        <div className="flex flex-col justify-center items-center gap-20  lg:w-2/3 w-full">
-
-                            <div className="flex justify-center  sm:text-4xl text-3xl font-bold text-themeColor ">
-                                Create your free account
-                            </div>
 
               <div className="flex md:w-2/3 w-full  gap-4  flex-col items-center  justify-center  ">
                 {current == 0 ? (
@@ -367,32 +287,6 @@ const OrganisationRegister = () => {
       </div>
     </>
   );
-
-
-                            <div className="flex gap-4 flex-col  justify-center items-center px-8 mb-10   w-full">
-                                <button className="px-6 py-3 rounded-md bg-blue-400/10 " >
-                                    <div className="flex gap-3 items-center ">
-                                        <FcGoogle />
-                                        <span>Register with Google</span>
-                                    </div>
-                                </button>
-                                <div className="flex font-semibold">
-                                    <div className="px-4 ">Already have an account? </div>
-                                    <Button color="default" variant="filled">
-                                        Sign in
-                                    </Button>
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
-
-                </ConfigProvider >
-            </div >
-
-        </>
-    );
 };
 
 export default OrganisationRegister;
