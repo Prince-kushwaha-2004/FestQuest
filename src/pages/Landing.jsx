041e43b6a408606f9d8be8a211372036e68ba4c8
Prince <prince121kk@gmail.com>
2025-01-27 16:36:22 +0530

    event preview page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import { CaretDownOutlined } from "@ant-design/icons";
 import { Dropdown, Space } from "antd";
-import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const LandingPage = () => {
   const NeumorphismButton = () => {
@@ -12,9 +12,7 @@ const LandingPage = () => {
           flex items-center gap-2 
           text-teal-700 text-lg
           shadow-[-5px_-5px_10px_rgba(255,_255,_255,_0.8),_5px_5px_10px_rgba(0,_0,_0,_0.25)]
-          
           transition-all
-  
           hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)]
           hover:text-teal-700
       `}
@@ -48,7 +46,7 @@ const LandingPage = () => {
     },
     {
       key: "2",
-      label: <Link to="login">Organisation</Link>,
+      label: <Link to="orgregister">Organisation</Link>,
     },
   ];
 
--- src/router/routing.jsx
+++ src/router/routing.jsx
@@ -4,6 +4,7 @@ import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import OrganisationRegister from "../pages/organisation/OrganisationRegister";
 import Register from "../pages/Register";
+import EventPreview from "../pages/User/EventPreview";
 import Feed from "../pages/User/Feed";
 import Root from "../Root";
 function Routing() {
@@ -15,6 +16,7 @@ function Routing() {
                 <Route path="register" element={<Register />} />
                 <Route path="Dashboard" element={<Dashboard />} >
                     <Route index element={<Feed />} />
+                    <Route path="event/:id" element={<EventPreview />} />
                 </Route>
                 <Route path="orgregister" element={<OrganisationRegister />} />
             </Route>
