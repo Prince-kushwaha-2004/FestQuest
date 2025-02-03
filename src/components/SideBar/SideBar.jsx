3c65c8e149c496a66fe2ef79f581404ef979f823
Suhani <suhaniss885@gmail.com>
2025-02-03 18:54:50 +0530

    update

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,161 +2,162 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { FaRegBookmark } from "react-icons/fa6";
 import {
-    FiChevronDown,
-    FiChevronsRight,
-    FiHome,
-    FiUsers,
+  FiChevronDown,
+  FiChevronsRight,
+  FiHome,
+  FiUsers,
 } from "react-icons/fi";
 import { IoMdDoneAll } from "react-icons/io";
 import { RiCalendarEventLine } from "react-icons/ri";
 import logo from "../../assets/festLogo.png";
 export const SideBar = () => {
-    return (
-        <div className="flex">
-            <Sidebar />
-        </div>
-    );
+  return (
+    <div className="flex">
+      <Sidebar />
+    </div>
+  );
 };
 
 const Sidebar = () => {
-    const [open, setOpen] = useState(true);
-    const [selected, setSelected] = useState("Dashboard");
+  const [open, setOpen] = useState(true);
+  const [selected, setSelected] = useState("Dashboard");
 
-    return (
-        <motion.nav
-            layout
-            className="absolute xl:sticky top-0 h-screen shrink-0 shadow-lg border-r backdrop-blur-3xl border-slate-300 p-2 z-10"
-            style={{
-                width: open ? "285px" : "fit-content",
-            }}
-        >
-            <TitleSection open={open} />
-            <div className="space-y-1">
-                <Option
-                    Icon={FiHome}
-                    title="Dashboard"
-                    selected={selected}
-                    setSelected={setSelected}
-                    open={open}
-                />
-                <Option
-                    Icon={IoMdDoneAll}
-                    title="Registered"
-                    selected={selected}
-                    setSelected={setSelected}
-                    open={open}
-                />
-                <Option
-                    Icon={FaRegBookmark}
-                    title="Saved"
-                    selected={selected}
-                    setSelected={setSelected}
-                    open={open}
-                />
-                <Option
-                    Icon={RiCalendarEventLine}
-                    title="My Events"
-                    selected={selected}
-                    setSelected={setSelected}
-                    open={open}
-                />
-                <Option
-                    Icon={FiUsers}
-                    title="Members"
-                    selected={selected}
-                    setSelected={setSelected}
-                    open={open}
-                />
-            </div>
+  return (
+    <motion.nav
+      layout
+      className="absolute xl:sticky top-0 h-screen shrink-0 shadow-lg border-r backdrop-blur-3xl border-slate-300 p-2 z-10"
+      style={{
+        width: open ? "285px" : "fit-content",
+      }}
+    >
+      <TitleSection open={open} />
+      <div className="space-y-1">
+        <Option
+          Icon={FiHome}
+          title="Dashboard"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={IoMdDoneAll}
+          title="Registered"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={FaRegBookmark}
+          title="Saved"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={RiCalendarEventLine}
+          title="My Events"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={FiUsers}
+          title="Create Event"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+      </div>
 
-            <ToggleClose open={open} setOpen={setOpen} />
-        </motion.nav>
-    );
+      <ToggleClose open={open} setOpen={setOpen} />
+    </motion.nav>
+  );
 };
 
 const Option = ({ Icon, title, selected, setSelected, open }) => {
-    return (
-        <motion.button
-            layout
-            onClick={() => setSelected(title)}
-            className={`relative flex h-10 w-full items-center rounded-2xl transition-colors ${selected === title
-                    ? "shadow bg-[#71f1cb11] border border-slate-300"
-                    : "text-slate-700 hover:bg-[#1d2f331e] hover:text-black"
-                }`}
-            style={{
-                padding: open ? "1.5rem" : "1.5rem 1rem",
-            }}
+  return (
+    <motion.button
+      layout
+      onClick={() => setSelected(title)}
+      className={`relative flex h-10 w-full items-center rounded-2xl transition-colors ${
+        selected === title
+          ? "shadow bg-[#71f1cb11] border border-slate-300"
+          : "text-slate-700 hover:bg-[#1d2f331e] hover:text-black"
+      }`}
+      style={{
+        padding: open ? "1.5rem" : "1.5rem 1rem",
+      }}
+    >
+      <motion.div
+        layout
+        className="grid h-full w-10 place-content-center text-xl"
+      >
+        <Icon />
+      </motion.div>
+      {open && (
+        <motion.span
+          layout
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.125 }}
+          className="text-lg"
         >
-            <motion.div
-                layout
-                className="grid h-full w-10 place-content-center text-xl"
-            >
-                <Icon />
-            </motion.div>
-            {open && (
-                <motion.span
-                    layout
-                    initial={{ opacity: 0, y: 12 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.125 }}
-                    className="text-lg"
-                >
-                    {title}
-                </motion.span>
-            )}
-        </motion.button>
-    );
+          {title}
+        </motion.span>
+      )}
+    </motion.button>
+  );
 };
 
 const TitleSection = ({ open }) => {
-    return (
-        <div className="mb-10 mt-4 pb-3">
-            <div className="flex cursor-pointer items-center justify-between shadow-md rounded-2xl transition-all hover:scale-103 hover:shadow-lg">
-                <div className="flex px-8 py-2 items-center gap-2">
-                    {open && (
-                        <motion.div
-                            layout
-                            initial={{ opacity: 0, y: 12 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ delay: 0.125 }}
-                        >
-                            <img src={logo} alt="" />
-                        </motion.div>
-                    )}
-                </div>
-                {open && <FiChevronDown className="mr-2" />}
-            </div>
+  return (
+    <div className="mb-10 mt-4 pb-3">
+      <div className="flex cursor-pointer items-center justify-between shadow-md rounded-2xl transition-all hover:scale-103 hover:shadow-lg">
+        <div className="flex px-8 py-2 items-center gap-2">
+          {open && (
+            <motion.div
+              layout
+              initial={{ opacity: 0, y: 12 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.125 }}
+            >
+              <img src={logo} alt="" />
+            </motion.div>
+          )}
         </div>
-    );
+        {open && <FiChevronDown className="mr-2" />}
+      </div>
+    </div>
+  );
 };
 const ToggleClose = ({ open, setOpen }) => {
-    return (
-        <motion.button
-            layout
-            onClick={() => setOpen((pv) => !pv)}
-            className="absolute bottom-0 left-0 right-0 border-t border-slate-300 transition-colors hover:bg-[#1d2f331e]"
+  return (
+    <motion.button
+      layout
+      onClick={() => setOpen((pv) => !pv)}
+      className="absolute bottom-0 left-0 right-0 border-t border-slate-300 transition-colors hover:bg-[#1d2f331e]"
+    >
+      <div className="flex items-center px-6 py-3">
+        <motion.div
+          layout
+          className="grid size-10 place-content-center text-xl"
         >
-            <div className="flex items-center px-6 py-3">
-                <motion.div
-                    layout
-                    className="grid size-10 place-content-center text-xl"
-                >
-                    <FiChevronsRight
-                        className={`transition-transform ${open && "rotate-180"}`}
-                    />
-                </motion.div>
-                {open && (
-                    <motion.span
-                        layout
-                        initial={{ opacity: 0, y: 12 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.125 }}
-                        className="text-lg font-medium"
-                    >
-                        Hide
-                    </motion.span>
-                )}
-            </div>
-        </motion.button>
-    );
+          <FiChevronsRight
+            className={`transition-transform ${open && "rotate-180"}`}
+          />
+        </motion.div>
+        {open && (
+          <motion.span
+            layout
+            initial={{ opacity: 0, y: 12 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.125 }}
+            className="text-lg font-medium"
+          >
+            Hide
+          </motion.span>
+        )}
+      </div>
+    </motion.button>
+  );
 };
--- src/pages/User/EventRegister.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventRegister = () => {
-  return <div>EventRegister</div>;
-};
-
-export default EventRegister;
