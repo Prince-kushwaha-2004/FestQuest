d62c422d78b11b47a8f2ab8ecc116e86bea6d5cb
Suhani <suhaniss885@gmail.com>
2025-02-05 12:26:23 +0530

    event form layout complete

--- src/components/StepForm/Form1.jsx
+++ src/components/StepForm/Form1.jsx
@@ -1,37 +1,34 @@
 import React from "react";
-import { InboxOutlined } from "@ant-design/icons";
+import { InboxOutlined, UploadOutlined } from "@ant-design/icons";
 import { Button, Form, Upload, Input, Select } from "antd";
 
 export const Basic = ({ formData, setFormData, current, setCurrent }) => {
   const { TextArea } = Input;
 
   const onFinish = (value) => {
-    console.log(value);
+    setFormData({ ...formData, basic: value });
+    console.log(formData);
     setCurrent(current + 1);
   };
 
   const onChange = (e) => {
     console.log("Change:", e.target.value);
   };
+
   return (
     <div className="md:mx-20">
-      <Form name="form" layout="vertical" onFinish={onFinish}>
+      <Form
+        name="form"
+        layout="vertical"
+        onFinish={onFinish}
+        initialValues={formData.basic}
+      >
         <Form.Item
           name="name"
           label="EVENT NAME"
           rules={[{ required: true, message: "Please enter the event name" }]}
         >
           <Input placeholder="Name of the event" />
-        </Form.Item>
-
-        <Form.Item
-          name="tagline"
-          label="TAGLINE"
-          rules={[
-            { required: true, message: "Please enter the event tagline" },
-          ]}
-        >
-          <Input placeholder="A tagline for your event" />
         </Form.Item>
 
         <Form.Item
@@ -48,19 +45,36 @@ export const Basic = ({ formData, setFormData, current, setCurrent }) => {
         </Form.Item>
 
         <Form.Item
+          name="type"
+          size="large"
+          label="EVENT TYPE"
+          rules={[{ required: true, message: "Please select the event type" }]}
+        >
+          <Select placeholder="Select type" size="large">
+            <Select.Option value="sports">Sports</Select.Option>
+            <Select.Option value="cultural">Cultural</Select.Option>
+            <Select.Option value="fintech">FinTech</Select.Option>
+            <Select.Option value="technical">Technical</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
           name="banner"
           label="EVENT BANNER"
-          valuePropName="fileList"
-          getValueFromEvent={(e) => e?.fileList}
+          // valuePropName="fileList"
+          //getValueFromEvent={(e) => e?.fileList}
+          rules={[
+            {
+              required: true,
+              message: "Please select a banner for your event",
+            },
+          ]}
         >
-          <Upload.Dragger style={{ backgroundColor: "white" }} name="bannerImg">
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined style={{ color: "teal" }} />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag Banner Image to this area to upload
-            </p>
-          </Upload.Dragger>
+          <Upload listType="picture" maxCount={1}>
+            <Button icon={<UploadOutlined />} size="large" block>
+              Upload Banner
+            </Button>
+          </Upload>
         </Form.Item>
 
         <div className="flex justify-center">
@@ -75,12 +89,18 @@ export const Basic = ({ formData, setFormData, current, setCurrent }) => {
 
 export const Details = ({ formData, setFormData, current, setCurrent }) => {
   const onFinish = (value) => {
-    console.log(value);
+    setFormData({ ...formData, eventDetails: value });
+    console.log(formData);
     setCurrent(current + 1);
   };
   return (
     <div className="md:mx-20">
-      <Form name="form" layout="vertical" onFinish={onFinish}>
+      <Form
+        name="form"
+        layout="vertical"
+        initialValues={formData.eventDetails}
+        onFinish={onFinish}
+      >
         <Form.Item
           name="mode"
           size="large"
@@ -89,47 +109,98 @@ export const Details = ({ formData, setFormData, current, setCurrent }) => {
             { required: true, message: "Please select the mode of your event" },
           ]}
         >
-          <Select placeholder="Select mode">
+          <Select placeholder="Select mode" size="large">
             <Select.Option value="online">Online</Select.Option>
             <Select.Option value="offline">Offline</Select.Option>
           </Select>
         </Form.Item>
 
-        <Form.Item
-          name="total"
-          label="APPROX. PARTICIPANTS"
+        {/* <Form.Item
+          name="websiteUrl"
+          label="EVENT WEBSITE URL"
           rules={[
-            { required: true, message: "Please enter approx participants" },
+            { required: true, message: "Please the URL of your event website" },
           ]}
         >
-          <Input type="number" placeholder="e.g. 200" />
-        </Form.Item>
+          <Input placeholder="URL for the event" />
+        </Form.Item> */}
+
+        <div className="flex flex-row justify-between">
+          <Form.Item
+            name="state"
+            size="large"
+            label="STATE"
+            style={{ width: "40%", marginRight: "10px" }}
+            rules={[{ required: true, message: "Please select state" }]}
+          >
+            <Select placeholder="Select state" size="large">
+              <Select.Option value="UP">UP</Select.Option>
+              <Select.Option value="MP">MP</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            name="city"
+            size="large"
+            label="CITY"
+            style={{ width: "40%" }}
+            rules={[{ required: true, message: "Please select city" }]}
+          >
+            <Select placeholder="Select city" size="large">
+              <Select.Option value="ghaziabad">Ghaziabad</Select.Option>
+              <Select.Option value="gurgaon">Gurgaon</Select.Option>
+            </Select>
+          </Form.Item>
+        </div>
 
         <Form.Item
-          name="min"
-          label="MIN. TEAM SIZE ALLOWED"
+          name="pin"
+          label="PINCODE"
           rules={[
             {
               required: true,
-              message: "Please enter the minimum members required for a team",
+              message: "Please enter the pincode",
             },
           ]}
         >
-          <Input type="number" min={2} max={5} placeholder="e.g. 2" />
+          <Input type="number" placeholder="Enter pincode" />
         </Form.Item>
 
         <Form.Item
-          name="max"
-          label="MAX. TEAM SIZE ALLOWED"
-          rules={[
-            {
-              required: true,
-              message: "Please enter the maximum members required for a team",
-            },
-          ]}
+          name="address"
+          label="EVENT ADDRESS"
+          rules={[{ required: true, message: "Please enter the address" }]}
         >
-          <Input type="number" min={4} max={15} placeholder="e.g. 4" />
+          <Input placeholder="Enter address" />
         </Form.Item>
+
+        <div className="flex flex-row justify-between">
+          <Form.Item
+            name="min"
+            label="MIN. TEAM SIZE ALLOWED"
+            rules={[
+              {
+                required: true,
+                message: "Please enter the minimum members required for a team",
+              },
+            ]}
+          >
+            <Input type="number" min={2} placeholder="e.g. 2" />
+          </Form.Item>
+
+          <Form.Item
+            name="max"
+            label="MAX. TEAM SIZE ALLOWED"
+            rules={[
+              {
+                required: true,
+                message: "Please enter the maximum members required for a team",
+              },
+            ]}
+          >
+            <Input type="number" min={2} placeholder="e.g. 4" />
+          </Form.Item>
+        </div>
 
         <div className="flex justify-center">
           <Button
